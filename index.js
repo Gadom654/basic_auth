53925de8d421fa6a940630a9c00f08ad58b4c0fd
Gadom <69717399+Gadom654@users.noreply.github.com>
2022-11-26 11:41:44 +0100

    some docs

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ var path = require('path');
 const app = express();
 
 function authentication(req, res, next) {
+	// Function that authenticate the users
 	var authheader = req.headers.authorization;
 	console.log(req.headers);
-
+	//Checking if user is authenticated
 	if (!authheader) {
 		var err = new Error('You are not authenticated!');
 		res.setHeader('WWW-Authenticate', 'Basic');
 		err.status = 401;
 		return next(err)
 	}
-
+	//vars to get login and password from user
 	var auth = new Buffer.from(authheader.split(' ')[1],
 	'base64').toString().split(':');
 	var user = auth[0];
 	var pass = auth[1];
-
+	//checking users login and password
 	if (user == 'lol' && pass == 'lol') {
-
+		// If user and password is correct go to next step, if not print the error
 		// If Authorized user
 		next();
 	} else {
@@ -32,7 +33,6 @@ function authentication(req, res, next) {
 	}
 
 }
-
 // First step is the authentication of the client
 app.use(authentication)
 app.use(express.static(path.join(__dirname, 'public')));
